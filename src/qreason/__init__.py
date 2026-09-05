feedd8586f84ef8d755d302b3573b97cc316fd99
Furox <real.furkannn@gmail.com>
2026-09-06 00:27:54 +0300

    Expose public package API

---
+++ src/qreason/__init__.py
@@ -0,0 +1,15 @@
+from .core import (
+    Branch,
+    EngineConfig,
+    Evaluation,
+    QuantumReasoningEngine,
+    ReasoningResult,
+)
+
+__all__ = [
+    "Branch",
+    "EngineConfig",
+    "Evaluation",
+    "QuantumReasoningEngine",
+    "ReasoningResult",
+]
